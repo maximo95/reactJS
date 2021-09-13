f4e15f42a4802683d48df771d24f14ef360be074
maximo95 <maximollallire@gmail.com>
2021-09-13 16:24:40 -0300

    desafio_clase_3

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import Nav from "./Nav"
+const App  = () => {
+    return (
+        <header className="test">
+            <h1>Tienda de Café</h1>
+            <nav/>
+        </header>
+    )
+}
+
+export default App
---
+++ src/nav.jsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return (
+        <nav>
+            <a href="">inicio</a>
+            <a href="">contacto</a>
+        </nav>
+    )
+} 
+
+export default Nav 
