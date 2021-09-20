180d3c1e24a5449124447075d169798b02e62b8b
maximo95 <maximollallire@gmail.com>
2021-09-20 19:27:30 -0300

    desafio5

--- src/ItemListContainer.jsx
+++ src/ItemListContainer.jsx
@@ -1,6 +1,19 @@
+import { useState,useEffect } from "react"
+
 const ItemListContainer = (saludo) => {
+    const [contador, setContador] = useState(0)
+
+    useEffect (()=> {
+        console.log("El componente se renderizó")
+    })
     return (
+        <>
         <p>{saludo}</p>
+        <p>Contador: {contador}</p>
+        <button onClick= {()=> {setContador(contador+1)}}>Aumentar Contador</button>
+        <button onClick= {()=> {setContador(contador-1)}}>Aumentar Contador</button>
+        <contador onAdd={()=>{}}/>
+        </>
     )
 }
 
--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+import { useState,useEffect} from "react"
 import ItemListContainer from "ItemListContainer"
 import Header from "./Header"
 const App  = () => {
