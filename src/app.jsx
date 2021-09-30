e91db1d42acc6a8530dd8dfb1ca1b3626d0013e9
maximo95 <maximollallire@gmail.com>
2021-09-29 23:15:21 -0300

    desafio_8

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,9 @@ const {Item} = Nav
 const App  = () => {
     return (
         <>
+        <Route path ="/" component={ItemListcontainer} exact/>
+        <Route  path ="/link/:id"  component={ItemListcontainer}/>
+        <Route  path ="/cart"  component={Cart}/>
         <Header id="main-header" titulo="Mi E-Commerce" subtitulo = "Tienda de Café"/>
         <ItemListContainer saludo ="Bienvenidos a mi E-Commerce"/>
         <Footer/>
